934a8e8532153d1d1dc74f8d47af5416376afc3f
ayham-eddin <ayham.alaaeddin.1@gmail.com>
2025-11-25 15:06:51 +0100

    feat: add frontend and backend

---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();  // Load .env FIRST
+
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { connectDB } from "./config/db";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Backend is running 🚀" });
+});
+
+const startServer = async () => {
+  console.log("Loaded PORT:", process.env.PORT);
+  console.log("Loaded URI:", process.env.MONGODB_URI);
+
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
